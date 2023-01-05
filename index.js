54060e29ac549e35330b49396b26a462a94f8ec6
Adam H.M <adamhasanmuzaki@gmail.com>
2023-01-05 19:06:07 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const exphbs = require('express-handlebars');
-const fileUpload = require('express-fileupload');
-const mysql = require('mysql');
+const express = require("express");
+const dotenv = require("dotenv");
+const cloudinary = require("cloudinary");
+const exphbs = require("express-handlebars");
+const fileUpload = require("express-fileupload");
+const mysql = require("mysql");
 
 dotenv.config();
 const app = express();
@@ -12,12 +13,12 @@ const port = 3000;
 app.use(fileUpload());
 
 // Static Files
-app.use(express.static('public'));
-app.use(express.static('upload'));
+app.use(express.static("public"));
+app.use(express.static("upload"));
 
 // Templating engine
-app.engine('hbs', exphbs({ extname: '.hbs' }));
-app.set('view engine', 'hbs');
+app.engine("hbs", exphbs({ extname: ".hbs" }));
+app.set("view engine", "hbs");
 
 // Connection Pool
 const pool = mysql.createPool({
@@ -25,64 +26,69 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
 });
 
 pool.getConnection((err, connection) => {
   if (err) throw err; // not connected
-  console.log('Connected!');
+  console.log("Connected!");
 });
 
-app.get('', (req, res) => {
+app.get("", (req, res) => {
   pool.getConnection((err, connection) => {
     if (err) throw err; // not connected
-    console.log('Connected!');
+    console.log("Connected!");
 
     connection.query('SELECT * FROM user WHERE id = "1"', (err, rows) => {
       // Once done, release connection
       connection.release();
       if (!err) {
-        res.render('index', { rows });
+        res.render("index", { rows });
       }
     });
-
   });
 });
 
-app.post('', (req, res) => {
-  let sampleFile;
-  let uploadPath;
-  
+cloudinary.config({
+  cloud_name: "ddwwbifgg",
+  api_key: "926681871762698",
+  api_secret: "RP046eeEZoFf7kBH6jk777vsiFw",
+});
+
+app.post("", async (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
+    return res.status(400).send("No files were uploaded.");
   }
   // name of the input is sampleFile
-  sampleFile = req.files.sampleFile;
-  let fileName = Date.now() + sampleFile.name;
-  uploadPath = __dirname + '/upload/' + fileName;
+  let file = req.files.sampleFile;
+  let fileName = Date.now() + file.name;
+  const result = await cloudinary.uploader.upload(file.tempFilePath, {
+    public_id: Date.now(),
+    resource_type: "auto",
+    folder: "images"
+  })
 
   // Use mv() to place file on the server
-  sampleFile.mv(uploadPath, function (err) {
-    if (err) return res.status(500).send(err);
+  if (err) return res.status(500).send(err);
 
-    pool.getConnection((err, connection) => {
-      if (err) throw err; // not connected
-      console.log('Connected!');
+  pool.getConnection((err, connection) => {
+    if (err) throw err; // not connected
+    console.log("Connected!");
 
-      connection.query('UPDATE user SET profile_image = ? WHERE id ="1"', [fileName], (err, rows) => {
+    connection.query(
+      'UPDATE user SET profile_image = ? WHERE id ="1"',
+      [result.url],
+      (err, rows) => {
         // Once done, release connection
         connection.release();
 
         if (!err) {
-          res.redirect('/');
+          res.redirect("/");
         } else {
           console.log(err);
         }
-
-      });
-    });
-
-    // res.send('File uploaded!');
+      }
+    );
   });
 });
 
